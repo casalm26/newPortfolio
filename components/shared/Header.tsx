a3f0f3abcfc61c4729392ce392fa2b5905f94e17
casalm26 <almerudcaspian@gmail.com>
2026-03-27 23:40:16 +0100

    GSC

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -34,6 +34,10 @@ const Header = ({ className }: { className?: string }) => {
         className,
       )}
     >
+      <meta
+        name="google-site-verification"
+        content="vmR3RFjWYvRqjp711FtJsa2vzyHUZ1QUaw_3iVDgrEI"
+      />
       <div className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between">
           {/* Logo */}
